f1ae665ee984e60e0ce8ced6327cf8a92411f4af
melyne <sollux.ex@gmail.com>
2024-12-05 23:26:19 +0100

    organ clickable

---
+++ components/popuptest.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>test de compo</p>
+    <button @click="yo">epic</button>
+  </template>
+  
+  <script setup lang="ts">
+  // Aucune logique spécifique n'est nécessaire pour l'instant
+ 
+  function yo(){
+    alert("yo")
+  }
+
+
+  </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
+
 <template>
-  <div>
-    <Home />
+  <div class="container mx-auto px-4 py-8">
+    <button @click="clicked">test</button>
+    <popuptest v-if="affiche"/>
+    <img src="~/public/sketch_peau.jpg"  @click="clicked">
   </div>
 </template>
+
+<script setup lang="ts">
+// Aucune logique spécifique n'est nécessaire pour l'instant
+import  popuptest from '~/components/popuptest.vue'
+const affiche=ref(false)
+const deaffiche=ref(0)
+function clicked(){
+  affiche.value=true
+  
+  if(deaffiche.value==1){
+    affiche.value=false
+    deaffiche.value=0  
+  }
+  else{
+    deaffiche.value=1
+  }
+  
+
+}
+
+
+</script>
