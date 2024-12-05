ac0039fea68d587a1c7de151b006421964aba846
Diamanteus <AlbanMartin29170@gmail.com>
2024-12-05 22:41:32 +0100

    test modif h2

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl font-bold mb-4 text-secondary">Nuit de l'Info 2024</h1>
     <p class="text-xl mb-6">On va parler de l'océan et des humains !</p>
     <div class="bg-accent-1 p-6 rounded-lg shadow-lg">
-      <h2 class="text-2xl font-semibold mb-4">Mes Projets Récents</h2>
+      <h2 class="text-2xl font-semibold mb-4">On va faire des mini-jeux</h2>
 
     </div>
   </div>
