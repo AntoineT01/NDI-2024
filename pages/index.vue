1a29a21dafcedf70ccc0d7bdba60f0573d0d4448
Diamanteus <AlbanMartin29170@gmail.com>
2024-12-05 22:08:24 +0100

    test changement index

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto px-4 py-8">
-    <h1 class="text-4xl font-bold mb-4 text-secondary">Bienvenue sur mon Portfolio</h1>
-    <p class="text-xl mb-6">Je suis un développeur passionné.</p>
+    <h1 class="text-4xl font-bold mb-4 text-secondary">Nuit de l'Info 2024</h1>
+    <p class="text-xl mb-6">On va parler de l'océan et des humains !</p>
     <div class="bg-accent-1 p-6 rounded-lg shadow-lg">
       <h2 class="text-2xl font-semibold mb-4">Mes Projets Récents</h2>
 
