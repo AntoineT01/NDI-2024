a2693ebb72250622b216705785aecc9e062eba43
Tanguy Le Goff <talegoff@asi.fr>
2024-12-06 01:14:24 +0100

    refacto

--- components/HumanBody.vue
+++ components/HumanBody.vue
@@ -1,27 +1,65 @@
 <script setup lang="ts">
-const display_heart = ref(true)
-const display_lungs = ref(false)
-const display_skin = ref(false)
+import { ref } from 'vue';
+import Organ from './Organ.vue';
 
-const heart_clicked = function () {
-  display_lungs.value = true
-}
+const organs = ref([
+  {
+    name: 'heart',
+    src: '/assets/human_body/sketch_coeur.png',
+    alt: 'Image d\'un coeur',
+    position: { top: '39%', left: '60%' },
+    size: '10%',
+    visible: true,
+    zIndex: 999,
+    next: 'lungs',
+  },
+  {
+    name: 'lungs',
+    src: '/assets/human_body/sketch_poumons.png',
+    alt: 'Image d\'un poumon',
+    position: { top: '37%', left: '57%' },
+    size: '30%',
+    visible: false,
+    zIndex: 1,
+    next: 'skin',
+  },
+  {
+    name: 'skin',
+    src: '/assets/human_body/sketch_peau.png',
+    alt: 'Image d\'une peau',
+    position: { top: '42%', left: '25%' },
+    size: '13%',
+    visible: false,
+    zIndex: 1,
+  },
+]);
 
-const lungs_clicked = function () {
-  display_skin.value = true
-}
-
-const skin_clicked = function () {
-  display_skin.value = true
-}
+const toggleOrgan = (currentOrgan: string) => {
+  const organ = organs.value.find((o) => o.name === currentOrgan);
+  if (organ && organ.next) {
+    const nextOrgan = organs.value.find((o) => o.name === organ.next);
+    if (nextOrgan) {
+      nextOrgan.visible = true;
+    }
+  }
+};
 </script>
 
 <template>
   <div class="human_container">
-    <img src="~/assets/human_body/sketch_personnage.png" alt="Image d'un humain" class="human_body">
-    <img src="~/assets/human_body/sketch_coeur.png" @click=heart_clicked alt="Image d'un coeur" class="human_heart">
-    <img src="~/assets/human_body/sketch_poumons.png" @click="lungs_clicked" v-if="display_lungs" alt="Image d'un poumon" class="human_lungs">
-    <img src="~/assets/human_body/sketch_peau.png" v-if="display_skin" alt="Image d'une peau" class="human_skin">
+    <img src="/assets/human_body/sketch_personnage.png" alt="Image d'un humain" class="human_body" />
+    <Organ
+        v-for="organ in organs"
+        :key="organ.name"
+        :name="organ.name"
+        :src="organ.src"
+        :alt="organ.alt"
+        :position="organ.position"
+        :size="organ.size"
+        :visible="organ.visible"
+        :zIndex="organ.zIndex"
+        :onClick="() => toggleOrgan(organ.name)"
+    />
   </div>
 </template>
 
@@ -36,36 +74,5 @@ const skin_clicked = function () {
 .human_body {
   width: 100%;
   height: 100%;
-}
-
-.human_lungs {
-  position: absolute;
-  top: 37%;
-  left: 57%;
-  transform: translate(-50%, -50%);
-  width: 30%;
-  height: auto;
-  cursor: pointer;
-}
-
-.human_heart {
-  position: absolute; /* Permet de positionner l'image par rapport au conteneur */
-  top: 39%; /* Ajuste verticalement, en partant du haut */
-  left: 60%; /* Ajuste horizontalement, en partant de la gauche */
-  transform: translate(-50%, -50%); /* Centre l'image au niveau du point défini par top et left */
-  width: 10%; /* Ajuste la taille du cœur (optionnel) */
-  height: auto; /* Conserve les proportions */
-  z-index: 999;
-  cursor: pointer;
-}
-.human_skin {
-  position: absolute;
-  top: 64%; /* Ajuste verticalement, en partant du haut */
-  left: 75%; /* Ajuste horizontalement, en partant de la gauche */
-  transform: translate(-50%, -50%); /* Centre l'image au niveau du point défini par top et left */
-  width: 14%; /* Ajuste la taille du cœur (optionnel) */
-  height: auto; /* Conserve les proportions */
-  z-index: 999;
-  cursor: pointer;
 }
 </style>
---
+++ components/Organ.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  src: String,
+  alt: String,
+  position: Object,
+  size: String,
+  visible: Boolean,
+  zIndex: Number,
+  onClick: Function,
+});
+</script>
+
+<template>
+  <img
+      v-if="visible"
+      :src="src"
+      :alt="alt"
+      :class="'human_' + name"
+      :style="{
+      top: position.top,
+      left: position.left,
+      width: size,
+      zIndex,
+      position: 'absolute',
+      transform: 'translate(-50%, -50%)',
+    }"
+      @click="onClick && onClick()"
+  />
+</template>
+
+<style scoped>
+img {
+  height: auto;
+  cursor: pointer;
+  transition: transform 0.3s ease, filter 0.3s ease;
+}
+
+img:hover {
+  transform: translate(-50%, -50%) scale(1.1) !important;
+  filter: brightness(1.3);
+}
+</style>
